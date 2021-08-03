b2c3c3570f9c8482f491a9de01a0663bfcc0f3fa
xyw6 <OxQ@me.com>
2021-08-04 03:46:18 +0800

    Update install_ss.sh

--- files/sh/install_ss.sh
+++ files/sh/install_ss.sh
@@ -18,7 +18,7 @@ _ss_tmp_gz="/tmp/ss-tmp/shadowsocks-go.gz"
 _ss_dir='/usr/bin/shadowsocks-go'
 _ss_file='/usr/bin/shadowsocks-go/shadowsocks-go'
 _ss_sh="/usr/local/sbin/ss"
-_ss_sh_link="https://zemin.life/sh/ss.sh"
+_ss_sh_link="https://zemin.life/files/sh/ss.sh"
 backup='/usr/bin/shadowsocks-go/backup.conf'
 
 # 笨笨的检测方法
